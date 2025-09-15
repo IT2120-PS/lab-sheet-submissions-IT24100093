3aaee97269f1e369df58d435a862f2624c988061
IT24100093 <it24100093@my.sliit.lk>
2025-09-16 00:58:58 +0530

    Add files via upload

---
+++ Lab Sheet 06/Lab6 R.R
@@ -0,0 +1,10 @@
+getwd()
+setwd("C:\\Users\\IT24100093\\Desktop\\IT24100093 ")
+#Q1
+#Binomal distribution
+pbinom(46, 50, 0.85,lower.tail = FALSE)
+
+#Q2
+#Number of calls received in given day
+#poisson distribution
+dpois(15, 12)
